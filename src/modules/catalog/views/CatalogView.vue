e0deee2db8d8eef5fb9b669a0445919ebf112057
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-10-24 23:43:27 +0500

    feat: implemented catalog with mocking

---
+++ src/modules/catalog/views/CatalogView.vue
@@ -0,0 +1,65 @@
+<script>
+    import ProductCard from '../components/ProductCard.vue'
+
+    export default {
+        name: 'CatalogView',
+        components: {
+            ProductCard
+        },
+        data() {
+            return {
+            products: [
+                {
+                    title: 'Пример товара 1',
+                    description: 'Описание товара 1',
+                    price: 1200,
+                    image: 'https://via.placeholder.com/200',
+                },
+                {
+                    title: 'Пример товара 2',
+                    description: 'Описание товара 2',
+                    price: 2300,
+                    image: 'https://via.placeholder.com/200',
+                },
+                {
+                    title: 'Пример товара 3',
+                    description: 'Описание товара 3',
+                    price: 1500,
+                    image: 'https://via.placeholder.com/200',
+                },
+                {
+                    title: 'Пример товара 4',
+                    description: 'Описание товара 4',
+                    price: 1700,
+                    image: 'https://via.placeholder.com/200',
+                },
+            ],
+            };
+        },
+    };
+</script>
+
+<template>
+    <div class="catalog-view">
+        <div class="product-list">
+            <ProductCard
+                v-for="(product, index) in products"
+                :key="index"
+                :product="product"
+            />
+        </div>
+    </div>
+</template>
+  
+<style scoped>
+    .catalog-view {
+        width: 90%;
+        margin: 0 auto;
+    }
+
+    .product-list {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+        gap: 5px;
+    }
+</style>
