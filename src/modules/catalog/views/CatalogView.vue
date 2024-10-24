e47be21fee0b39261d6488eaa8372cba54e86bfb
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-10-25 00:00:39 +0500

    feat: added detailed product page from catalog routing

--- src/modules/catalog/views/CatalogView.vue
+++ src/modules/catalog/views/CatalogView.vue
@@ -10,24 +10,28 @@
             return {
             products: [
                 {
+                    id: 1,
                     title: 'Пример товара 1',
                     description: 'Описание товара 1',
                     price: 1200,
                     image: 'https://via.placeholder.com/200',
                 },
                 {
+                    id: 2,
                     title: 'Пример товара 2',
                     description: 'Описание товара 2',
                     price: 2300,
                     image: 'https://via.placeholder.com/200',
                 },
                 {
+                    id: 3,
                     title: 'Пример товара 3',
                     description: 'Описание товара 3',
                     price: 1500,
                     image: 'https://via.placeholder.com/200',
                 },
                 {
+                    id: 4,
                     title: 'Пример товара 4',
                     description: 'Описание товара 4',
                     price: 1700,
@@ -42,11 +46,15 @@
 <template>
     <div class="catalog-view">
         <div class="product-list">
-            <ProductCard
+            <router-link
                 v-for="(product, index) in products"
                 :key="index"
-                :product="product"
-            />
+                :to="{ name: 'product_detail', params: { id: product.id } }"
+            >
+
+            <ProductCard :product="product" />
+
+            </router-link>
         </div>
     </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import DefaultLayout from '../core/layouts/DefaultLayout.vue';
 import Catalog from '../modules/catalog/views/CatalogView.vue'
+import ProductDetailPage from '../modules/product/views/ProductDetailPage.vue';
 
-const routes = [{
-    path: "/",
+
+const routes = [
+    {
+        path: "/",
+        component: DefaultLayout,
+        children: [
+        {
+            path: "/",
+            name: "catalog",
+            component: Catalog,
+        },
+        ]
+  },
+  {
+    path: "/product/:id",
     component: DefaultLayout,
     children: [
-      {
-        path: "/",
-        name: "catalog",
-        component: Catalog,
-      },
+    {
+        path: "/product/:id",
+        name: "product_detail",
+        component: ProductDetailPage,
+        props: true,
+    },
     ]
-  }];
+},
+];
 
 const router = createRouter({
     history: createWebHistory(),
